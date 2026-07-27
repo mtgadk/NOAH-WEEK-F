9f15ded0d0010eef6c2084dfc7fbc9a6b4e1818c
mtgadk <noah.byun@mastersny.org>
2026-07-27 11:10:08 -0400

    Calc.py

--- Calc.py
+++ Calc.py
@@ -1,15 +1,19 @@
-print ("input 2 numbers")
-x=input ()
-x=int(x)
-y=input()
-y=float(y)
-print ("do you want to use subtraction, multiplaction, addition, or division?")
-z=input ()
-if z== ("subtraction"):
-    print (x-y)
-if z== ("multiplication"):
-    print (x*y)
-if z== ("addition"):
-    print (x+y)
-if z== ("division"):
-    print (x/y)
+looping=True
+while looping==True:
+    print ("input 2 numbers")
+    x=input ()
+    x=int(x)
+    y=input()
+    y=float(y)
+    print ("do you want to use subtraction, multiplaction, addition, or division or quit?")
+    z=input ()
+    if z== ("subtraction"):
+        print (x-y)
+    if z== ("multiplication"):
+        print (x*y)
+    if z== ("addition"):
+        print (x+y)
+    if z== ("division"):
+        print (x/y)
+    if z== ("quit"):
+        looping=False
