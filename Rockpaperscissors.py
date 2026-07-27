67b0f45d29c2cc07102298d600d33f93f72a88da
mtgadk <noah.byun@mastersny.org>
2026-07-27 11:01:43 -0400

    Rock Paper Scissors Game Updated

--- Rockpaperscissors.py
+++ Rockpaperscissors.py
@@ -1,38 +1,55 @@
-
 from random import randint
-cpuChoice = randint(0, 2)
-if cpuChoice==0:
-    cpuChoice= ("rock")
-if cpuChoice==1:
-    cpuChoice= ("paper")
-if cpuChoice==2:
-   cpuChoice= ("scissors")
-print ("Rock paper or sciccors")
-x=input ()
-if x==0:
-    print ("rock")
-if x==1:
-    print ("paper")
-if x==2:
-    print ("scissors")
-print (cpuChoice)
-if  x==("rock") and cpuChoice==("paper"):
-    print ("You lose")
-if x==("paper") and cpuChoice==("paper"):
-    print ("tie")
-if x==("scissors") and cpuChoice==("paper"):
-    print ("you win")
-if x==("rock") and cpuChoice==("rock"):
-    print ("tie")
-if x==("paper") and cpuChoice==("rock"):
-    print ("you win")
-if x==("scissors") and cpuChoice==("rock"):
-    print ("you lose")
-if x== ("rock") and cpuChoice== ("scissors"):
-    print ("you win")
-if x== ("paper") and cpuChoice== ("scissors"):
-    print ("you lose")
-if x== ("scissors") and cpuChoice== ("scissors"):
-    print ("tie")
 
+looping = True
+games=0
+playerscore=0
+cpuscore=0
+while looping == True: 
+    cpuChoice = randint(0, 2)
+    if cpuChoice==0:
+        cpuChoice= ("rock")
+    if cpuChoice==1:
+        cpuChoice= ("paper")
+    if cpuChoice==2:
+        cpuChoice= ("scissors")
+    print ("Rock paper sciccors or quit")
+    x=input ()
+    if x==0:
+        print ("rock")
+    if x==1:
+        print ("paper")
+    if x==2:
+        print ("scissors")
+    if x==("quit"):
+        looping=False
+    print (cpuChoice)
 
+    if  x==("rock") and cpuChoice==("paper"):
+        print ("You lose haha")
+        cpuscore=cpuscore+1
+    if x==("paper") and cpuChoice==("paper"):
+        print ("tie")
+    if x==("scissors") and cpuChoice==("paper"):
+        print ("you win")
+        playerscore=playerscore+1
+    if x==("rock") and cpuChoice==("rock"):
+        print ("tie")
+    if x==("paper") and cpuChoice==("rock"):
+        print ("you win")
+        playerscore=playerscore+1
+    if x==("scissors") and cpuChoice==("rock"):
+        print ("you lose haha")
+        cpuscore=cpuscore+1
+    if x== ("rock") and cpuChoice== ("scissors"):
+        print ("you win")
+        playerscore=playerscore+1
+    if x== ("paper") and cpuChoice== ("scissors"):
+        print ("you LOSE haha")
+        cpuscore=cpuscore+1
+    if x== ("scissors") and cpuChoice== ("scissors"):
+        print ("tie")
+    games=games+1
+    print (games)
+    print (cpuscore)
+    print (playerscore)
+    
