c64445ba9379aee5f665a451b27d85e3ff7dc814
mtgadk <noah.byun@mastersny.org>
2026-07-29 09:33:35 -0400

    Fibonacci

---
+++ Lists.py
@@ -0,0 +1,12 @@
+numbers = []
+#Code that adds every number 1-100 to the list
+x=0
+looping=True
+while looping==True:
+         x=x+1
+         numbers.append (x)
+        
+         if x==100000:
+            looping=False
+
+print(numbers) 
