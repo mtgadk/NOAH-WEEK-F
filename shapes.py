51ff7cf922d71dcc5b4252121937a050c1ecf4c2
mtgadk <noah.byun@mastersny.org>
2026-07-31 09:54:50 -0400

    aaa

--- shapes.py
+++ shapes.py
@@ -2,8 +2,17 @@ print ("what do you want your shape size to be")
 userinput=input()
 userinput=int (userinput)
 for i in range (1,userinput+1,1):
-for i in range ()
-    print ("*" end "")
-
-  
+    for i in range (1,userinput+1,1):
+        print ("* ", end= "")
     print()
+
+
+x=1
+print ("what do you want your shape size to be")
+userinput=input()
+userinput=int (userinput)
+for i in range (1,userinput+1,1):
+    for i in range (1,x+1,1):
+        print ("* ", end= "")
+    print()
+    x=x+1
